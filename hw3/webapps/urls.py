8c5053efca7121f3e2f6ba1b1a5e360d97d39c3b
Evan Palmer <evansp01@gmail.com>
2015-01-29 01:42:29 -0500

    changed urls to comply with spec

--- hw3/webapps/urls.py
+++ hw3/webapps/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^calculator$', 'calculator.views.calculator')
+    url(r'^$', 'calculator.views.calculator')
 )
