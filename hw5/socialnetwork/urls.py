8914b718d9aeafc6347bbb3ef5c5659a81b335a6
Evan Palmer <evansp01@gmail.com>
2015-02-19 00:03:57 -0500

    changed url patterns to have prefix for convenience

--- hw5/socialnetwork/urls.py
+++ hw5/socialnetwork/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
-    url(r'^$', 'socialnetwork.views.home'),
+urlpatterns = patterns('socialnetwork.views',
+    url(r'^$', 'home'),
     # Route for built-in authentication with our own custom login page
+    url(r'^register$', 'register'),
+    url(r'^following$', 'following'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_\-\.]*)$','profile'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_\-\.]*)/image$','get_image'),
+    url(r'^editprofile$','edit'),
+)
+
+urlpatterns += patterns('',
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'socialnetwork/login.html'}),
     # Route to logout a user and send them back to the login page
     url(r'^logout$', 'django.contrib.auth.views.logout_then_login'),
-    url(r'^register$', 'socialnetwork.views.register'),
-    url(r'^following$', 'socialnetwork.views.following'),
-    url(r'^profile/(?P<username>[a-zA-Z0-9_\-\.]*)$','socialnetwork.views.profile'),
-)
+    )
 
