451b3b77725bc4935c13eaf18aff5ff664f06c2d
Evan Palmer <evansp01@gmail.com>
2015-02-22 14:50:35 -0500

    added views for ajax

--- hw6/socialnetwork/views_other.py
+++ hw6/socialnetwork/views_other.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
+from django.template.loader import render_to_string
+from django.middleware.csrf import get_token
+import json
+
 
 # Decorator to use built-in authentication system
 from django.contrib.auth.decorators import login_required
@@ -19,6 +23,8 @@ from socialnetwork.models import *
 from socialnetwork.forms import *
 
 
+
+
 @login_required
 def render_global(request, form):
     items = Post.objects.all().order_by('-date')
@@ -28,6 +34,7 @@ def render_global(request, form):
     context['global_posts'] = len(Post.objects.all())
     context['user_posts'] = len(Post.objects.filter(user=request.user))
     context['form'] = form
+    context['cform'] = CommentForm()
     return render(request, 'socialnetwork/home_feed.html', context)
 
 
@@ -35,17 +42,16 @@ def render_global(request, form):
 def render_following(request):
     items = []
     # because who needs to know how to query the database properly
-    following = request.user.userprofile.follows.all()
-    for profile in following:
-        items.extend(Post.objects.filter(user=profile.userkey))
-    items.sort(key=lambda x: x.date)
-    items.reverse()
+    items = Post.objects.filter(
+                 user__in=request.user.userprofile.follows.all(),             
+                 ).order_by('-date')
     context = {}
     context['items'] = items
-    context['following'] = following
+    context['following'] = request.user.userprofile.follows.all()
     context['user'] = request.user
     context['global_posts'] = len(Post.objects.all())
     context['user_posts'] = len(Post.objects.filter(user=request.user))
+    context['cform'] = CommentForm()
     return render(request, 'socialnetwork/following_feed.html', context)
 
 
@@ -61,6 +67,47 @@ def home(request):
     else:
         return render_global(request, PostForm())
 
+def home_feed(request):
+    if request.GET:
+        value = -1
+        if 'last' in request.GET:
+            value = int(request.GET['last'])
+        items = Post.objects.all().order_by('-date')
+        top = items[0].id
+        index = 0
+        for i, item in enumerate(items):
+            print "id ", item.id, "value ", value
+            if int(item.id) == int(value):
+                index = i
+                break
+        items = items[0:index]
+        csrf_token_value = get_token(request)
+        feed = render_to_string("socialnetwork/item.html",{"items":items,
+            "csrf_token": csrf_token_value, "cform":CommentForm()})
+        response = {"html":feed,"last": top}
+        jsond = json.dumps(response)
+        return HttpResponse(jsond, content_type='application/json')
+
+
+def comment(request, item_id):
+    if request.POST:
+        form = CommentForm(request.POST);
+        if form.is_valid():
+            comment = Comment(text = form.cleaned_data['text'],
+                              user = request.user)
+            comment.save()
+            post = Post.objects.get(id=item_id)
+            post.comments.add(comment)
+            post.save()
+            print "happy"
+            return render(request, 'socialnetwork/comments.html', {"item":post})
+        else:
+            return page_not_found(request)
+
+    return HttpResponse(
+        "{}",
+        content_type="application/json"
+    )
 
 @login_required
 @transaction.atomic
