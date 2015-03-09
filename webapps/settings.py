8d4d620fb4fc192cec1e4b487468119c94b61611
Ubuntu <ubuntu@ip-172-31-56-142.ec2.internal>
2015-03-09 08:21:13 +0000

    changed to postgresql

--- webapps/settings.py
+++ webapps/settings.py
@@ -69,10 +69,21 @@ WSGI_APPLICATION = 'webapps.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ubuntu',
+        'USER': 'ubuntu',
+        'PASSWORD': private_settings.DB_PASS,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
